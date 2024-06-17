16866181aaff88d1296d200b57019dda72ef4c3c
Yuri Martins <yuri.martins@sou.unaerp.edu.br>
2024-06-17 06:40:25 -0300

    Update busca_termos.dart

--- app_odonto/lib/View/busca_termos.dart
+++ app_odonto/lib/View/busca_termos.dart
@@ -19,6 +19,7 @@ class BuscaTermos extends StatefulWidget {
 
 class _BuscaTermosState extends State<BuscaTermos> {
   bool status = true;
+  String nome = "";
 
   var txtNome = TextEditingController();
   var txtDescricao = TextEditingController();
@@ -49,6 +50,11 @@ class _BuscaTermosState extends State<BuscaTermos> {
                           onPressed: (){}, 
                           )
                       ),
+                      onChanged: (val){
+                        setState(() {
+                          nome = val;
+                        });
+                      },
           ),
           const SizedBox(
                       height: 10,
@@ -82,87 +88,14 @@ class _BuscaTermosState extends State<BuscaTermos> {
                         itemBuilder: (context, index) {
                           String id = dados.docs[index].id;
                           dynamic doc = dados.docs[index].data();
-                          return Card(
-                            // Set the shape of the card using a rounded rectangle border with a 8 pixel radius
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(8),
-                            ),
-                            // Set the clip behavior of the card
-                            clipBehavior: Clip.antiAliasWithSaveLayer,
-                            // Define the child widgets of the card
-                            child: InkWell(
-                              hoverColor: const Color.fromARGB(50, 25, 195, 207),
-                              onTap: () {
-                                Clipboard.setData(ClipboardData(text: '${doc['nome']}\n${doc['descricao']}'));
-                                sucesso(context, 'Termo Técnico copiado com sucesso!');
-                              },
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: <Widget>[
-                                  // Add a container with padding that contains the card's title, text, and buttons
-                                  Container(
-                                    padding: const EdgeInsets.fromLTRB(15, 15, 15, 0),
-                                    child: Column(
-                                      crossAxisAlignment: CrossAxisAlignment.start,
-                                      children: <Widget>[
-                                        // Display the card's title using a font size of 24 and a dark grey color
-                                        Text(
-                                          doc['nome'],
-                                          style: const TextStyle(
-                                            fontSize: 24,
-                                            color: Color.fromARGB(255, 10, 16, 83),
-                                          ),
-                                        ),
-                                        // Add a space between the title and the text
-                                        Container(height: 10),
-                                        // Display the card's text using a font size of 15 and a light grey color
-                                        Text(
-                                          doc['descricao'],
-                                          style: TextStyle(
-                                            fontSize: 15,
-                                            color: Colors.grey[700],
-                                          ),
-                                        ),
-                              
-                                        Container(height: 5),
-                                        // Add a row with two buttons spaced apart and aligned to the right side of the card
-                                        Row(
-                                          children: <Widget>[
-                                            // Add a spacer to push the buttons to the right side of the card
-                                            const Spacer(),
-                                            // Add a text button labeled "SHARE" with transparent foreground color and an accent color for the text
-                                            Visibility(
-                                              visible: perfilProfessor,
-                                              child: IconButton(
-                                                onPressed: () {
-                                                  txtNome.text = doc['nome'];
-                                                  txtDescricao.text = doc['descricao'];
-                                                  status = doc['status'];
-                                                  salvarTermoTecnico(context, docId: id);
-                                                  },
-                                                  icon: const Icon(Icons.edit_outlined),
-                                              ),
-                                            ),
-                                            // Add a text button labeled "EXPLORE" with transparent foreground color and an accent color for the text
-                                            Visibility(
-                                              visible: perfilProfessor,
-                                              child: IconButton(
-                                                onPressed: () {
-                                                  TermosTecnicosController().excluir(context, id);
-                                                },
-                                                icon: const Icon(Icons.delete_outlined),
-                                              ),
-                                            ),
-                                          ],
-                                        ),
-                                        Container(height: 5),
-                                      ],
-                                    ),
-                                  )
-                                  ]
-                              ),
-                            )
-                          );
+                          
+                          if(nome.isEmpty){
+                            return buildCard(doc, id);
+                          }
+                          else if(doc['nome'].toString().toLowerCase().contains(nome.toLowerCase())){
+                            return buildCard(doc, id);
+                          }
+                          return Container();
                         }
                       );
                     } else {
@@ -294,5 +227,89 @@ class _BuscaTermosState extends State<BuscaTermos> {
     );
   }
   );
+  }
+
+  Widget buildCard(doc, id){
+    return Card(
+                            // Set the shape of the card using a rounded rectangle border with a 8 pixel radius
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            // Set the clip behavior of the card
+                            clipBehavior: Clip.antiAliasWithSaveLayer,
+                            // Define the child widgets of the card
+                            child: InkWell(
+                              hoverColor: const Color.fromARGB(50, 25, 195, 207),
+                              onTap: () {
+                                Clipboard.setData(ClipboardData(text: '${doc['nome']}\n${doc['descricao']}'));
+                                sucesso(context, 'Termo Técnico copiado com sucesso!');
+                              },
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: <Widget>[
+                                  // Add a container with padding that contains the card's title, text, and buttons
+                                  Container(
+                                    padding: const EdgeInsets.fromLTRB(15, 15, 15, 0),
+                                    child: Column(
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: <Widget>[
+                                        // Display the card's title using a font size of 24 and a dark grey color
+                                        Text(
+                                          doc['nome'],
+                                          style: const TextStyle(
+                                            fontSize: 24,
+                                            color: Color.fromARGB(255, 10, 16, 83),
+                                          ),
+                                        ),
+                                        // Add a space between the title and the text
+                                        Container(height: 10),
+                                        // Display the card's text using a font size of 15 and a light grey color
+                                        Text(
+                                          doc['descricao'],
+                                          style: TextStyle(
+                                            fontSize: 15,
+                                            color: Colors.grey[700],
+                                          ),
+                                        ),
+                              
+                                        Container(height: 5),
+                                        // Add a row with two buttons spaced apart and aligned to the right side of the card
+                                        Row(
+                                          children: <Widget>[
+                                            // Add a spacer to push the buttons to the right side of the card
+                                            const Spacer(),
+                                            // Add a text button labeled "SHARE" with transparent foreground color and an accent color for the text
+                                            Visibility(
+                                              visible: perfilProfessor,
+                                              child: IconButton(
+                                                onPressed: () {
+                                                  txtNome.text = doc['nome'];
+                                                  txtDescricao.text = doc['descricao'];
+                                                  status = doc['status'];
+                                                  salvarTermoTecnico(context, docId: id);
+                                                  },
+                                                  icon: const Icon(Icons.edit_outlined),
+                                              ),
+                                            ),
+                                            // Add a text button labeled "EXPLORE" with transparent foreground color and an accent color for the text
+                                            Visibility(
+                                              visible: perfilProfessor,
+                                              child: IconButton(
+                                                onPressed: () {
+                                                  TermosTecnicosController().excluir(context, id);
+                                                },
+                                                icon: const Icon(Icons.delete_outlined),
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                        Container(height: 5),
+                                      ],
+                                    ),
+                                  )
+                                  ]
+                              ),
+                            )
+                          );
   }
 }
